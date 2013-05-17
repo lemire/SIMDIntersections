630c55929d0c3207fb99e5513c109b3526b1b06c
Daniel Lemire <lemire@gmail.com>
2013-05-16 20:51:00 -0400

    Intel compiler is not quite Intel compliant

--- include/intersectionfactory.h
+++ include/intersectionfactory.h
@@ -12,11 +12,48 @@
 #include "mediumintersection.h"
 #include "widevectorintersection.h"
 
-std::map<std::string,std::string> hhh = {
-    {   "schlegel","sfds"},
-    {   "danschlegel","sfds"}
-};
+#ifdef __INTEL_COMPILER
+/**
+ * Intel does not really support C++11 so we have to do something ugly like this:
+ */
 
+std::map<std::string,cardinalityintersectionfunction> initializefactory() {
+    std::map<std::string,cardinalityintersectionfunction> schemes;
+    schemes[ "danhybrid" ] =  danielshybridintersectioncardinality;
+    schemes[ "widevector" ] =  widevector_cardinality_intersect;
+    schemes[ "danscalar" ] =  intersectioncardinality;
+    schemes[ "galloping" ] =  frogintersectioncardinality;
+    schemes[ "1sgalloping" ] =  onesidedgallopingintersectioncardinality;
+    schemes[ "textbook" ] =  classicalintersectioncardinality;
+    schemes[ "textbook2" ] =  highlyscalablewordpresscom::cardinality_intersect_scalar;
+    schemes[ "hssimd" ] =  highlyscalablewordpresscom::cardinality_intersect_SIMD;
+    schemes[ "danhssimd" ] =  highlyscalablewordpresscom::opti2_cardinality_intersect_SIMD;
+    schemes[ "natemedium" ] =  nate_count_medium;
+    schemes[ "natedanmedium" ] =  natedan_count_medium;
+    schemes[ "natedanaltmedium" ] =  natedanalt_count_medium;
+    schemes[ "danfarmedium" ] =  danfar_count_medium;
+    schemes[ "danfarfinemedium" ] =  danfarfine_count_medium;
+    return schemes;
+}
+
+
+std::map<std::string,cardinalityintersectionfunction> schemes = initializefactory();
+
+std::map<std::string,cardinalityintersectionfunctionpart> initializefactorypart() {
+    std::map<std::string,cardinalityintersectionfunctionpart> partschemes;
+    partschemes[ "schlegel" ] = partitioned::cardinality_intersect_partitioned;
+    partschemes[ "danschlegel" ] = partitioned::faster_cardinality_intersect_partitioned;
+    return partschemes;
+}
+
+
+std::map<std::string,cardinalityintersectionfunctionpart> partschemes = initializefactorypart();
+
+#else
+
+/**
+ * This is the proper way to do it:
+ */
 std::map<std::string,cardinalityintersectionfunction> schemes = {
 	{	"danhybrid",danielshybridintersectioncardinality},
 	{	"widevector", widevector_cardinality_intersect},
@@ -36,12 +73,12 @@ std::map<std::string,cardinalityintersectionfunction> schemes = {
 
 
 
-
 std::map<std::string,cardinalityintersectionfunctionpart> partschemes = {
 	{	"schlegel",partitioned::cardinality_intersect_partitioned},
 	{	"danschlegel", partitioned::faster_cardinality_intersect_partitioned}
 };
 
+#endif
 
 /**
  * Convenience function
