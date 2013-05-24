548ec1158b70c82fb55c5e80a074142bd7a977c1
Nathan Kurz <nate@verse.com>
2013-05-23 22:45:13 -0700

    None working check point

--- intersection3.c
+++ intersection3.c
@@ -89,16 +89,108 @@ size_t search_chunks(const uint32_t *freq, const size_t lenFreq,
     
     uint32_t nextMatch = *rare;
     VECTYPE NextMatch = SETALL(nextMatch);
+    goto CHECK_FREQ;
 
-    while (usually(freq < stopFreq)) {
-            
+    do {
+        ASSERT(maxChunk >= nextMatch);
+
+        VECTYPE Q0, Q1, Q2, Q3; // quarters
+        VECTYPE S0, S1; // semis
+        VECTYPE F0; // final
+
+        VECTYPE Match = NextMatch;
+        if (usually(rare < lastRare)) { // PROFILE: verify compare is shared
+            nextMatch = rare[1];      // with (rare <= last) in loop header
+            NextMatch = SETALL(nextMatch);
+        }
+        rare += 1;   
+
+
+#if (MAXCHUNK >= 1)
+        uint32_t jump = 0;  // help compiler see the cmov
+        if (sometimes(nextMatch > maxChunk)) { // PROFILE: verify cmov
+            jump = CHUNKINTS;  
+        }        
+        freq += jump;
+#if (MAXCHUNK >= 2)
+        jump = 0;
+        if (unlikely(nextMatch > maxChunk2)) { // PROFILE: verify cmov
+            jump = CHUNKINTS;  
+        }        
+        freq += jump;
+#endif // MAXCHUNK >= 2
+#endif // MAXCHUNK >= 1
+
+        COMPILER_BARRIER;
+
+        M0 = _mm_cmpeq_epi32(M0, Match);
+        M1 = _mm_cmpeq_epi32(M1, Match);
+        Q0 = _mm_or_si128(M0, M1);
+        maxChunk = freq[CHUNKINTS - 1];
+#if (MAXCHUNK >= 2)
+        maxChunk2 = freq[2 * CHUNKINTS - 1]; 
+#endif // MAXCHUNK >= 2
+
+        COMPILER_BARRIER;
+
+        M2 = _mm_cmpeq_epi32(M2, Match);
+        M3 = _mm_cmpeq_epi32(M3, Match);
+        Q1 = _mm_or_si128(M2, M3);
+        M0 = _mm_load_si128((VECTYPE *) freq + 0);
+        M1 = _mm_load_si128((VECTYPE *) freq + 1);
+
+        COMPILER_BARRIER;
+
+        M4 = _mm_cmpeq_epi32(M4, Match);
+        M5 = _mm_cmpeq_epi32(M5, Match); 
+        Q2 = _mm_or_si128(M4, M5);
+        M2 = _mm_load_si128((VECTYPE *) freq + 2);
+        M3 = _mm_load_si128((VECTYPE *) freq + 3);
+
+        COMPILER_BARRIER;
+
+        M6 = _mm_cmpeq_epi32(M6, Match);
+        M7 = _mm_cmpeq_epi32(M7, Match);
+        Q3 = _mm_or_si128(M6, M7);
+        M4 = _mm_load_si128((VECTYPE *) freq + 4);
+        M5 = _mm_load_si128((VECTYPE *) freq + 5);
+
+        COMPILER_BARRIER;
+
+        S0 = _mm_or_si128(Q0, Q1);
+        S1 = _mm_or_si128(Q2, Q3);
+        F0 = _mm_or_si128(S0, S1);
+        M6 = _mm_load_si128((VECTYPE *) freq + 6);
+        M7 = _mm_load_si128((VECTYPE *) freq + 7);
+
+        COMPILER_BARRIER;
+
+        if (! _mm_testz_si128(F0, F0)) {
+            count += 1;             // PROFILE: verify cmov
+        }
+
+        // Exit if we have already checked lastRare
+        if (rarely(rare > lastRare)) {
+            ASSERT(rare = lastRare + 1);
+            return count; // no scalar finish after lastRare done
+        }
+
+        
+    CHECK_FREQ:
+        if (rarely(freq >= stopFreq)) {
+            const uint32_t lenFreq = lastFreq - freq + 1;
+            const uint32_t lenRare = lastRare - rare + 1;
+            return count + finish_scalar(freq, lenFreq, rare, lenRare);
+        }
+
         uint32_t maxChunk = freq[CHUNKINTS - 1];
 
-        if (sometimes(maxChunk < nextMatch)) {
-            freq += CHUNKINTS;  
-            continue;  
-        }  
+        if (rarely(maxChunk < nextMatch)) {
+            freq += CHUNKINTS;
+            goto CHECK_FREQ;
+        }
 
+        // FIXME:  need load to happen iff loop was taken
         VECTYPE M0 = LOAD((VECTYPE *)freq + 0); 
         VECTYPE M1 = LOAD((VECTYPE *)freq + 1); 
         VECTYPE M2 = LOAD((VECTYPE *)freq + 2); 
@@ -107,114 +199,15 @@ size_t search_chunks(const uint32_t *freq, const size_t lenFreq,
         VECTYPE M5 = LOAD((VECTYPE *)freq + 5); 
         VECTYPE M6 = LOAD((VECTYPE *)freq + 6); 
         VECTYPE M7 = LOAD((VECTYPE *)freq + 7); 
-
+        
 #if (MAXCHUNK >= 2)
         uint32_t maxChunk2 = freq[2 * CHUNKINTS - 1]; 
 #endif // MAXCHUNK >= 2
-            
-        do {
-            ASSERT(maxChunk >= nextMatch);
+        }
 
-            VECTYPE Q0, Q1, Q2, Q3; // quarters
-            VECTYPE S0, S1; // semis
-            VECTYPE F0; // final
-
-            VECTYPE Match = NextMatch;
-            if (usually(rare < lastRare)) { // PROFILE: verify compare is shared
-                nextMatch = rare[1];      // with (rare <= last) in loop header
-                NextMatch = SETALL(nextMatch);
-            }
-            rare += 1;   
-
-
-#if (MAXCHUNK >= 1)
-            uint32_t jump = 0;  // help compiler see the cmov
-            if (sometimes(nextMatch > maxChunk)) { // PROFILE: verify cmov
-                jump = CHUNKINTS;  
-            }        
-            freq += jump;
-#if (MAXCHUNK >= 2)
-            jump = 0;
-            if (unlikely(nextMatch > maxChunk2)) { // PROFILE: verify cmov
-                jump = CHUNKINTS;  
-            }        
-            freq += jump;
-#endif // MAXCHUNK >= 2
-#endif // MAXCHUNK >= 1
-
-            COMPILER_BARRIER;
-
-            M0 = _mm_cmpeq_epi32(M0, Match);
-            M1 = _mm_cmpeq_epi32(M1, Match);
-            Q0 = _mm_or_si128(M0, M1);
-            maxChunk = freq[CHUNKINTS - 1];
-#if (MAXCHUNK >= 2)
-            maxChunk2 = freq[2 * CHUNKINTS - 1]; 
-#endif // MAXCHUNK >= 2
-
-            COMPILER_BARRIER;
-
-            M2 = _mm_cmpeq_epi32(M2, Match);
-            M3 = _mm_cmpeq_epi32(M3, Match);
-            Q1 = _mm_or_si128(M2, M3);
-            M0 = _mm_load_si128((VECTYPE *) freq + 0);
-            M1 = _mm_load_si128((VECTYPE *) freq + 1);
-
-            COMPILER_BARRIER;
-
-            M4 = _mm_cmpeq_epi32(M4, Match);
-            M5 = _mm_cmpeq_epi32(M5, Match); 
-            Q2 = _mm_or_si128(M4, M5);
-            M2 = _mm_load_si128((VECTYPE *) freq + 2);
-            M3 = _mm_load_si128((VECTYPE *) freq + 3);
-
-            COMPILER_BARRIER;
-
-            M6 = _mm_cmpeq_epi32(M6, Match);
-            M7 = _mm_cmpeq_epi32(M7, Match);
-            Q3 = _mm_or_si128(M6, M7);
-            M4 = _mm_load_si128((VECTYPE *) freq + 4);
-            M5 = _mm_load_si128((VECTYPE *) freq + 5);
-
-            COMPILER_BARRIER;
-
-            S0 = _mm_or_si128(Q0, Q1);
-            S1 = _mm_or_si128(Q2, Q3);
-            F0 = _mm_or_si128(S0, S1);
-            M6 = _mm_load_si128((VECTYPE *) freq + 6);
-            M7 = _mm_load_si128((VECTYPE *) freq + 7);
-
-            COMPILER_BARRIER;
-
-            if (! _mm_testz_si128(F0, F0)) {
-                count += 1;             // PROFILE: verify cmov
-            }
-
-            // Exit if we have checked the last rare
-            if (rarely(rare > lastRare)) {
-                ASSERT(rare = lastRare + 1);
-                return count; // no scalar finish after lastRare done
-            }
-
-            // Exit if the conditionals advanced freq to/past stopFreq
-            if (rarely(freq >= stopFreq)) {
-                const uint32_t lenFreq = lastFreq - freq + 1;
-                const uint32_t lenRare = lastRare - rare + 1;
-                return count + finish_scalar(freq, lenFreq, rare, lenRare);
-            }
-
-            // NOTE: the correct vectors usually are already preloaded 
-        } while (usually(maxChunk >= nextMatch));
-
-            // exiting inner loop requires further increment of freq
-            freq += CHUNKINTS;
     }
-    // exit from outer loop means we reached stopFreq
-    // FIXME: names
-    freq -= CHUNKINTS; // undue the last increment that put us over?
-    const uint32_t lenF = lastFreq - freq + 1;
-    const uint32_t lenR = lastRare - rare + 1;
-    return count + finish_scalar(freq, lenF, rare, lenR);
+    // NOTREACHED
+    return count;
 }
 
   
