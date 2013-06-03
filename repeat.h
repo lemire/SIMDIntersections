198155307141c817e6fb9571daebe569222548d0
Nathan Kurz <nate@verse.com>
2013-06-03 04:12:42 -0700

    More additions

--- repeat.h
+++ repeat.h
@@ -21,11 +21,11 @@
 
 // INTERNALS
 
-#define _REPEAT_ADD_ONE(n, ignore) n + 1
+#define _REPEAT_ADD_ONE(n, ignore) _REPEAT_ADD_ONE_ ## n
 #define _REPEAT_INCREMENT(n, inc) n + inc
 #define _REPEAT_UNCHANGED(unchanged, ignore) unchanged
 
-#define _REPEAT_1(m, n, f, i, a...) m(n, ## a) 
+#define _REPEAT_1(macro, n, func, i, args...) macro(n, ## args) 
 #define _REPEAT_2(m, n, f, i, a...) m(n, ## a); _REPEAT_1(m, f(n, i), f, i, ## a)
 #define _REPEAT_3(m, n, f, i, a...) m(n, ## a); _REPEAT_2(m, f(n, i), f, i, ## a)
 #define _REPEAT_4(m, n, f, i, a...) m(n, ## a); _REPEAT_3(m, f(n, i), f, i, ## a)
@@ -58,14 +58,64 @@
 #define _REPEAT_31(m, n, f, i, a...) m(n, ## a); _REPEAT_30(m, f(n, i), f, i, ## a)
 #define _REPEAT_32(m, n, f, i, a...) m(n, ## a); _REPEAT_31(m, f(n, i), f, i, ## a)
 
+#define _REPEAT_ADD_ONE_0 1
+#define _REPEAT_ADD_ONE_1 2
+#define _REPEAT_ADD_ONE_2 3
+#define _REPEAT_ADD_ONE_3 4
+#define _REPEAT_ADD_ONE_4 5
+#define _REPEAT_ADD_ONE_5 6
+#define _REPEAT_ADD_ONE_6 7
+#define _REPEAT_ADD_ONE_7 8
+#define _REPEAT_ADD_ONE_8 9
+#define _REPEAT_ADD_ONE_9 10
+#define _REPEAT_ADD_ONE_10 11
+#define _REPEAT_ADD_ONE_11 12
+#define _REPEAT_ADD_ONE_12 13
+#define _REPEAT_ADD_ONE_13 14
+#define _REPEAT_ADD_ONE_14 15
+#define _REPEAT_ADD_ONE_15 16
+#define _REPEAT_ADD_ONE_16 17
+#define _REPEAT_ADD_ONE_17 18
+#define _REPEAT_ADD_ONE_18 19
+#define _REPEAT_ADD_ONE_19 20
+#define _REPEAT_ADD_ONE_20 21
+#define _REPEAT_ADD_ONE_21 22
+#define _REPEAT_ADD_ONE_22 23
+#define _REPEAT_ADD_ONE_23 24
+#define _REPEAT_ADD_ONE_24 25
+#define _REPEAT_ADD_ONE_25 26
+#define _REPEAT_ADD_ONE_26 27
+#define _REPEAT_ADD_ONE_27 28
+#define _REPEAT_ADD_ONE_28 29
+#define _REPEAT_ADD_ONE_29 30
+#define _REPEAT_ADD_ONE_30 31
+#define _REPEAT_ADD_ONE_31 32
+
+// FUTURE: find a more generic manner of cycling?
+// FUTURE: add cycles of different lengths
+
+#define REPEAT_CYCLE(macro, times, start_n, cycle_len, macro_args... )   \
+    REPEAT(macro, times, start_n, REPEAT_CYCLE ## cycle_len ## _NEXT, 0, ## macro_args)
+
+#define REPEAT_CYCLE4_NEXT(n, ignore...) _REPEAT_CYCLE4_NEXT_ ## n
+#define _REPEAT_CYCLE4_NEXT_0 1
+#define _REPEAT_CYCLE4_NEXT_1 2
+#define _REPEAT_CYCLE4_NEXT_2 3
+#define _REPEAT_CYCLE4_NEXT_3 0
+
+#define REPEAT_CYCLE4_PREV(n, ignore...) _REPEAT_CYCLE4_PREV_ ## n
+#define _REPEAT_CYCLE4_PREV_0 3
+#define _REPEAT_CYCLE4_PREV_1 0
+#define _REPEAT_CYCLE4_PREV_2 1
+#define _REPEAT_CYCLE4_PREV_3 2
+
 
 #ifdef MAIN
-
 #include <stdio.h>
 
 #define PRINT(x) printf("%d\n", x)
-
 #define NOARGS() printf("noargs\n")
+#define PASTE(x, base) paste(base ## x)
 
 int main() {
     REPEAT_ADD_ONE(PRINT, 32, 0);
@@ -73,8 +123,9 @@ int main() {
     REPEAT_NOARGS(NOARGS, 5);
     REPEAT_BLOCK({PRINT(1); PRINT(2)}, 3);
     REPEAT_SAME_ARGS(printf, 4, "functions OK too\n");
-    uint32_t var = 1;
-    REPEAT_ADD_ONE(PRINT, 32, n);
+    uint32_t var = 3;  REPEAT_INCREMENT(PRINT, 5, var, 3);
+    REPEAT_ADD_ONE(PASTE, 4, 0, var);
+    REPEAT_CYCLE(PRINT, 8, 0, 4);
 }
 
 #endif
