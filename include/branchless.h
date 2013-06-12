a66e1cf85149b5073345f4a9feb0d96629aba433
Nathan Kurz <nate@verse.com>
2013-06-11 19:17:22 -0700

    Mention that the cached versions read past end of array

--- include/branchless.h
+++ include/branchless.h
@@ -84,6 +84,7 @@ size_t scalar_branchless(const uint32_t *A, size_t lenA,
     return count;
 }
 
+// NOTE: Proof of concept function --- reads past end of input
 size_t scalar_branchless_cached(const uint32_t *A, size_t lenA, 
                                 const uint32_t *B, size_t lenB,
                                 uint32_t *Match) {
@@ -99,14 +100,14 @@ size_t scalar_branchless_cached(const uint32_t *A, size_t lenA,
         
 #ifdef IACA
         IACA_START;
-#endif
-        *Match = thisA;   // write the result regardless of match
-    
+#endif    
         uint32_t nextA = A[1];
         uint32_t nextB = B[1];
 
         uint32_t oldA = thisA;
         uint32_t oldB = thisB;
+
+        *Match = thisA;   // write the result regardless of match
 
         int m = (oldB == oldA) ? 1 : 0;  // advance Match only if equal
         int a = (oldB >= oldA) ? 1 : 0;  // advance A if match or B ahead
@@ -130,6 +131,7 @@ size_t scalar_branchless_cached(const uint32_t *A, size_t lenA,
     return count; 
 }
 
+// NOTE: Proof of concept function --- reads past end of input
 size_t scalar_branchless_cached2(const uint32_t *A, size_t lenA, 
                                  const uint32_t *B, size_t lenB,
                                  uint32_t *Match) {
@@ -149,13 +151,13 @@ size_t scalar_branchless_cached2(const uint32_t *A, size_t lenA,
         IACA_START;
 #endif
 
-        *Match = thisA; // write the result regardless of match
-
         uint32_t nextNextA = A[2];
         uint32_t nextNextB = B[2];
 
         uint32_t oldA = thisA;
         uint32_t oldB = thisB;
+
+        *Match = thisA; // write the result regardless of match
 
         int m = (oldB == oldA) ? 1 : 0;  // advance Match only if equal
         int a = (oldB >= oldA) ? 1 : 0;  // advance A if match or B ahead
