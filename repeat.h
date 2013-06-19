5d367aac2139a068f56874fdaee9359602467995
Nathan Kurz <nate@verse.com>
2013-06-18 22:47:03 -0700

    New interface mostly working

--- repeat.h
+++ repeat.h
@@ -1,65 +1,93 @@
 #define REPEAT(macro, times, start_n, next_func, next_arg, macro_args...) \
+    _REPEAT_EXPAND(_REPEAT_ ## times(macro, start_n, next_func, next_arg, ## macro_args))
+#define REPEAT_INNER(macro, times, start_n, next_func, next_arg, macro_args...) \
     _REPEAT_ ## times(macro, start_n, next_func, next_arg, ## macro_args)
 
-#define REPEAT_ADD_ONE(macro, times, start_n, macro_args... )                    \
-    REPEAT(macro, times, start_n, _REPEAT_ADD_ONE, 0, ## macro_args)
 
-#define REPEAT_ADDING(macro, times, start_n, increment, macro_args...)                 \
+#define REPEAT_ADD_ONE(macro, times, start_n, macro_args... )           \
+    REPEAT(macro, times, start_n, _REPEAT_ADD_ONE, 0, ## macro_args)
+#define REPEAT_ADD_ONE_INNER(macro, times, start_n, macro_args... )     \
+    REPEAT_INNER(macro, times, start_n, _REPEAT_ADD_ONE, 0, ## macro_args)
+
+
+#define REPEAT_ADDING(macro, times, start_n, increment, macro_args...)  \
     REPEAT(macro, times, start_n, _REPEAT_ADDING, increment, ## macro_args)
 
-#define REPEAT_SAME_ARGS(macro, times, first_macro_arg, other_macro_args...)      \
+#define REPEAT_SAME_ARGS(macro, times, first_macro_arg, other_macro_args...) \
     REPEAT(macro, times, first_macro_arg, _REPEAT_UNCHANGED, 0, ## other_macro_args)
 
-#define REPEAT_SAME_ARG(macro, times, first_macro_arg)      \
+#define REPEAT_SAME_ARG(macro, times, first_macro_arg)          \
     REPEAT(macro, times, first_macro_arg, _REPEAT_UNCHANGED, 0)
 
-#define REPEAT_NOARGS(macro, times)      \
+
+#define REPEAT_NO_ARGS(macro, times)            \
     REPEAT(macro, times,, _REPEAT_UNCHANGED, 0)
+#define REPEAT_NO_ARGS_INNER(macro, times)      \
+    REPEAT_INNER(macro, times,, _REPEAT_UNCHANGED, 0)
+
 
 #define REPEAT_BLOCK(block, times)              \
     REPEAT(do block while, times, 0, _REPEAT_UNCHANGED, 0)
 
+#define REPEAT_APPEND_LITERAL(macro, times, initial, literal)           \
+    REPEAT(macro, times, initial, _REPEAT_APPEND_LITERAL, literal)
+
+// FUTURE: REPEAT_SUB, REPEAT_MUL, REPEAT_DIV
 #define REPEAT_ADD(x, y) _REPEAT_ADDING(x, y)
 
 // INTERNALS
 
+#define _REPEAT_EMPTY()
+#define _REPEAT_DEFER(token) token _REPEAT_EMPTY()
+
+#define _REPEAT_EXPAND(args...) _REPEAT_EXPAND_9(args)
+#define _REPEAT_EXPAND_9(args...) _REPEAT_EXPAND_3(_REPEAT_EXPAND_3(_REPEAT_EXPAND_3(args)))
+#define _REPEAT_EXPAND_3(args...) _REPEAT_EXPAND_1(_REPEAT_EXPAND_1(_REPEAT_EXPAND_1(args)))
+#define _REPEAT_EXPAND_1(args...) args
+
+// For REPEAT using a variable number of arguments instead
+// http://stackoverflow.com/questions/1872220/is-it-possible-to-iterate-over-arguments-in-variadic-macros
+
+
 #define _REPEAT_ADDING(n, inc) _REPEAT_ADD_ ## n(inc)
 #define _REPEAT_ADD_ONE(n, ignore...) _REPEAT_ADD_ONE_ ## n
 #define _REPEAT_UNCHANGED(unchanged, ignore...) unchanged
+#define _REPEAT_APPEND_LITERAL(n, literal) n literal
+
 
 #define _REPEAT_0(args...)  /* empty */
-#define _REPEAT_1(macro, n, func, i, args...) macro(n, ## args) 
-#define _REPEAT_2(m, n, f, i, a...) m(n, ## a); _REPEAT_1(m, f(n, i), f, i, ## a)
-#define _REPEAT_3(m, n, f, i, a...) m(n, ## a); _REPEAT_2(m, f(n, i), f, i, ## a)
-#define _REPEAT_4(m, n, f, i, a...) m(n, ## a); _REPEAT_3(m, f(n, i), f, i, ## a)
-#define _REPEAT_5(m, n, f, i, a...) m(n, ## a); _REPEAT_4(m, f(n, i), f, i, ## a)
-#define _REPEAT_6(m, n, f, i, a...) m(n, ## a); _REPEAT_5(m, f(n, i), f, i, ## a)
-#define _REPEAT_7(m, n, f, i, a...) m(n, ## a); _REPEAT_6(m, f(n, i), f, i, ## a)
-#define _REPEAT_8(m, n, f, i, a...) m(n, ## a); _REPEAT_7(m, f(n, i), f, i, ## a)
-#define _REPEAT_9(m, n, f, i, a...) m(n, ## a); _REPEAT_8(m, f(n, i), f, i, ## a)
-#define _REPEAT_10(m, n, f, i, a...) m(n, ## a); _REPEAT_9(m, f(n, i), f, i, ## a)
-#define _REPEAT_11(m, n, f, i, a...) m(n, ## a); _REPEAT_10(m, f(n, i), f, i, ## a)
-#define _REPEAT_12(m, n, f, i, a...) m(n, ## a); _REPEAT_11(m, f(n, i), f, i, ## a)
-#define _REPEAT_13(m, n, f, i, a...) m(n, ## a); _REPEAT_12(m, f(n, i), f, i, ## a)
-#define _REPEAT_14(m, n, f, i, a...) m(n, ## a); _REPEAT_13(m, f(n, i), f, i, ## a)
-#define _REPEAT_15(m, n, f, i, a...) m(n, ## a); _REPEAT_14(m, f(n, i), f, i, ## a)
-#define _REPEAT_16(m, n, f, i, a...) m(n, ## a); _REPEAT_15(m, f(n, i), f, i, ## a)
-#define _REPEAT_17(m, n, f, i, a...) m(n, ## a); _REPEAT_16(m, f(n, i), f, i, ## a)
-#define _REPEAT_18(m, n, f, i, a...) m(n, ## a); _REPEAT_17(m, f(n, i), f, i, ## a)
-#define _REPEAT_19(m, n, f, i, a...) m(n, ## a); _REPEAT_18(m, f(n, i), f, i, ## a)
-#define _REPEAT_20(m, n, f, i, a...) m(n, ## a); _REPEAT_19(m, f(n, i), f, i, ## a)
-#define _REPEAT_21(m, n, f, i, a...) m(n, ## a); _REPEAT_20(m, f(n, i), f, i, ## a)
-#define _REPEAT_22(m, n, f, i, a...) m(n, ## a); _REPEAT_21(m, f(n, i), f, i, ## a)
-#define _REPEAT_23(m, n, f, i, a...) m(n, ## a); _REPEAT_22(m, f(n, i), f, i, ## a)
-#define _REPEAT_24(m, n, f, i, a...) m(n, ## a); _REPEAT_23(m, f(n, i), f, i, ## a)
-#define _REPEAT_25(m, n, f, i, a...) m(n, ## a); _REPEAT_24(m, f(n, i), f, i, ## a)
-#define _REPEAT_26(m, n, f, i, a...) m(n, ## a); _REPEAT_25(m, f(n, i), f, i, ## a)
-#define _REPEAT_27(m, n, f, i, a...) m(n, ## a); _REPEAT_26(m, f(n, i), f, i, ## a)
-#define _REPEAT_28(m, n, f, i, a...) m(n, ## a); _REPEAT_27(m, f(n, i), f, i, ## a)
-#define _REPEAT_29(m, n, f, i, a...) m(n, ## a); _REPEAT_28(m, f(n, i), f, i, ## a)
-#define _REPEAT_30(m, n, f, i, a...) m(n, ## a); _REPEAT_29(m, f(n, i), f, i, ## a)
-#define _REPEAT_31(m, n, f, i, a...) m(n, ## a); _REPEAT_30(m, f(n, i), f, i, ## a)
-#define _REPEAT_32(m, n, f, i, a...) m(n, ## a); _REPEAT_31(m, f(n, i), f, i, ## a)
+#define _REPEAT_1(macro, n, func, i, args...) _REPEAT_DEFER(macro)(n, ## args) 
+#define _REPEAT_2(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_1(m, f(n, i), f, i, ## a)
+#define _REPEAT_3(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_2(m, f(n, i), f, i, ## a)
+#define _REPEAT_4(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_3(m, f(n, i), f, i, ## a)
+#define _REPEAT_5(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_4(m, f(n, i), f, i, ## a)
+#define _REPEAT_6(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_5(m, f(n, i), f, i, ## a)
+#define _REPEAT_7(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_6(m, f(n, i), f, i, ## a)
+#define _REPEAT_8(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_7(m, f(n, i), f, i, ## a)
+#define _REPEAT_9(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_8(m, f(n, i), f, i, ## a)
+#define _REPEAT_10(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_9(m, f(n, i), f, i, ## a)
+#define _REPEAT_11(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_10(m, f(n, i), f, i, ## a)
+#define _REPEAT_12(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_11(m, f(n, i), f, i, ## a)
+#define _REPEAT_13(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_12(m, f(n, i), f, i, ## a)
+#define _REPEAT_14(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_13(m, f(n, i), f, i, ## a)
+#define _REPEAT_15(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_14(m, f(n, i), f, i, ## a)
+#define _REPEAT_16(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_15(m, f(n, i), f, i, ## a)
+#define _REPEAT_17(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_16(m, f(n, i), f, i, ## a)
+#define _REPEAT_18(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_17(m, f(n, i), f, i, ## a)
+#define _REPEAT_19(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_18(m, f(n, i), f, i, ## a)
+#define _REPEAT_20(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_19(m, f(n, i), f, i, ## a)
+#define _REPEAT_21(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_20(m, f(n, i), f, i, ## a)
+#define _REPEAT_22(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_21(m, f(n, i), f, i, ## a)
+#define _REPEAT_23(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_22(m, f(n, i), f, i, ## a)
+#define _REPEAT_24(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_23(m, f(n, i), f, i, ## a)
+#define _REPEAT_25(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_24(m, f(n, i), f, i, ## a)
+#define _REPEAT_26(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_25(m, f(n, i), f, i, ## a)
+#define _REPEAT_27(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_26(m, f(n, i), f, i, ## a)
+#define _REPEAT_28(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_27(m, f(n, i), f, i, ## a)
+#define _REPEAT_29(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_28(m, f(n, i), f, i, ## a)
+#define _REPEAT_30(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_29(m, f(n, i), f, i, ## a)
+#define _REPEAT_31(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_30(m, f(n, i), f, i, ## a)
+#define _REPEAT_32(m, n, f, i, a...) _REPEAT_DEFER(m)(n, ## a); _REPEAT_31(m, f(n, i), f, i, ## a)
 
 #define _REPEAT_ADD_ONE_0 1
 #define _REPEAT_ADD_ONE_1 2
@@ -128,7 +156,6 @@
 #define _REPEAT_ADD_31(x) _REPEAT_ADD_30(_REPEAT_ADD_ONE(x))
 
 
-
 // FUTURE: find a more generic manner of cycling?
 // FUTURE: add cycles of different lengths
 
@@ -148,25 +175,69 @@
 #define _REPEAT_CYCLE4_PREV_3 2
 
 
-#ifdef MAIN
-#include <stdio.h>
-#include <stdint.h>
 
-#define PRINT(x) printf("%d\n", x)
-#define NOARGS() printf("noargs\n")
-#define PASTE(x, base) uint32_t base ## x
+#ifdef MAIN
+
+int printf(const char *format, ...);
+
+#define PRINTD(x) printf("%d\n", x)
+void simple_add_one() {
+    REPEAT_ADD_ONE(PRINTD, 16, 1);
+}
+
+#define NUMOUTER 2
+#define NUMINNER 3
+#define INNER(freqnum, rarenum) printf("(%d, %d)\n", freqnum, rarenum)
+#define OUTER(n) REPEAT_ADD_ONE_INNER(INNER, NUMINNER, 1, n)
+void recursive_add_one(void) {
+    REPEAT_ADD_ONE(OUTER, NUMOUTER, 0);
+}
+
+#define NO_ARGS() printf("noargs\n")
+void no_args() {
+    REPEAT_NO_ARGS(NO_ARGS, 5);
+}
+
+void repeat_adding() {
+    REPEAT_ADDING(PRINTD, 16, 0, 2);
+}
+
+void same_args() {
+    REPEAT_SAME_ARGS(printf, 4, "functions OK too\n");
+}
+
+void block() {
+    REPEAT_BLOCK({PRINTD(1); PRINTD(2);}, 3);
+}
+
+#define DECLARE(x, base) int base ## x = x
+void declare() {
+    REPEAT_ADD_ONE(DECLARE, 3, 0, var);
+    printf("sum: %d\n", var0 + var1 + var2);
+}
+
+void cycle4() {
+    REPEAT_CYCLE(PRINTD, 8, 0, 4);
+}
+
+#define PRINTS(s) printf("%s\n", s)
+void literal(int var) {
+    REPEAT_APPEND_LITERAL(PRINTD, 4, var, + 3);
+    REPEAT_APPEND_LITERAL(PRINTS, 3, "foo", "bar");
+}
 
 int main() {
-    REPEAT_ADD_ONE(PRINT, 32, 0);
-    REPEAT_ADDING(PRINT, 16, 0, 2);
-    REPEAT_NOARGS(NOARGS, 5);
-    REPEAT_BLOCK({PRINT(1); PRINT(2);}, 3);
-    REPEAT_SAME_ARGS(printf, 4, "functions OK too\n");
-//    uint32_t var = 3;  REPEAT_ADDING(PRINT, 5, var, 3);
-    REPEAT_ADD_ONE(PASTE, 4, 0, var);
-    REPEAT_CYCLE(PRINT, 8, 0, 4);
-
+    recursive_add_one();
+    simple_add_one();
+    no_args();
+    repeat_adding();
+    same_args();
+    block();
+    declare();
+    cycle4();
+    int var = 2; literal(var); 
     return 0;
 }
+
 
 #endif
