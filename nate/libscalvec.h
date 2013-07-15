18c7e947619199db1c551c8882192d9de8b9362f
nkurz <nate@verse.com>
2013-07-15 02:24:06 -0400

    Partially manual use of libscalvec

---
+++ nate/libscalvec.h
@@ -0,0 +1,177 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+size_t match_scalar(const uint32_t *A, const size_t lenA,
+                    const uint32_t *B, const size_t lenB,
+                    uint32_t *out);
+       
+
+
+    size_t match_scalvec_v4_f1_a0
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+
+
+
+
+
+
+    size_t match_scalvec_v4_f2_a0
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+
+
+
+
+
+
+
+    size_t match_scalvec_v4_f3_a0
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+
+
+
+
+
+
+
+    size_t match_scalvec_v4_f4_a0
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f5_a0
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f6_a0
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f7_a0
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f8_a0
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f2_a1
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+
+
+
+
+
+
+
+    size_t match_scalvec_v4_f3_a1
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+
+
+
+
+
+
+
+    size_t match_scalvec_v4_f4_a1
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f5_a1
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f6_a1
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f7_a1
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f8_a1
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f3_a2
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+
+
+
+
+
+
+
+    size_t match_scalvec_v4_f4_a2
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f5_a2
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f6_a2
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f7_a2
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f8_a2
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f4_a3
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f5_a3
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f6_a3
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f7_a3
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f8_a3
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f5_a4
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f6_a4
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f7_a4
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+    size_t match_scalvec_v4_f8_a4
+         (const uint32_t *rare, size_t lenRare,
+          const uint32_t *freq, size_t lenFreq,
+          uint32_t *matchOut);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
