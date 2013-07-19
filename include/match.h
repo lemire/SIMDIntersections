99999102c032f9ce8a4da57815d55461c642ef12
Daniel Lemire <lemire@gmail.com>
2013-07-19 15:56:16 -0400

    Very small change to make it possible to build with clang

--- include/match.h
+++ include/match.h
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 #include <stdint.h>
 #include <stddef.h>
@@ -7,7 +7,7 @@
 #define VEC_T __m128i
 
 #define VEC_COPY_LOW(reg_dest, xmm_src)                                 \
-    __asm volatile("movd %1, %0" : "=r" (reg_dest) : "x" (xmm_src))       
+    __asm volatile("movd %1, %0" : "=r" (reg_dest) : "x" (xmm_src))
 
 #define VEC_OR(dest, other)                                             \
     __asm volatile("por %1, %0" : "+x" (dest) : "x" (other) )
@@ -26,14 +26,14 @@
 
 #define VEC_CMP_EQUAL(dest, other)                                      \
     __asm volatile("pcmpeqd %1, %0" : "+x" (dest) : "x" (other))
-    
+
 #define VEC_SET_ALL_TO_INT(reg, int32)                                  \
     __asm volatile("movd %1, %0; pshufd $0, %0, %0"                 \
-                       : "=x" (reg) : "g" (int32) ) 
+                       : "=x" (reg) : "g" (int32) )
 
 #define VEC_LOAD_OFFSET(xmm, ptr, bytes)                    \
     __asm volatile("movdqu %c2(%1), %0" : "=x" (xmm) :  \
-                   "g" (ptr), "i" (bytes))
+                   "r" (ptr), "i" (bytes))
 
 #define COMPILER_LIKELY(x)     __builtin_expect((x),1)
 #define COMPILER_RARELY(x)     __builtin_expect((x),0)
@@ -52,7 +52,7 @@ extern "C" {
 #endif
 
 size_t match_scalar(const uint32_t *A, const size_t lenA,
-                    const uint32_t *B, const size_t lenB, 
+                    const uint32_t *B, const size_t lenB,
                     uint32_t *out);
 
 
