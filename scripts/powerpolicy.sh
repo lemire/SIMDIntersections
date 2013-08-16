8f9db2ac64d37cae0056aaf6833a008e806161ca
Daniel Lemire <dlemire@fastpfor.(none)>
2013-08-16 16:13:12 -0400

    Fixed typo

--- scripts/powerpolicy.sh
+++ scripts/powerpolicy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# take from http://hbfs.wordpress.com/2013/06/18/fast-path-finding-part-ii/
+# taken from http://hbfs.wordpress.com/2013/06/18/fast-path-finding-part-ii/
 # invoke with performance or ondemand 
 # enumerate found CPUs
 cpus=$( grep processor /proc/cpuinfo | cut -d: -f 2 )
