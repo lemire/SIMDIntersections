f7d5e951697ea27863d1efa453eb07b19488911a
Daniel Lemire <dlemire@fastpfor.(none)>
2013-08-16 16:28:00 -0400

    Updated script

--- scripts/powerpolicy.sh
+++ scripts/powerpolicy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # taken from http://hbfs.wordpress.com/2013/06/18/fast-path-finding-part-ii/
 # invoke with performance or ondemand 
+# type cpufreq-info to check results
 # enumerate found CPUs
 cpus=$( grep processor /proc/cpuinfo | cut -d: -f 2 )
  
@@ -8,5 +9,5 @@ cpus=$( grep processor /proc/cpuinfo | cut -d: -f 2 )
 #
 for cpu in ${cpus[@]}
 do
- cpufreq-selector -c $cpu -g $1
+  cpufreq-set -c $cpu -g $1
 done
