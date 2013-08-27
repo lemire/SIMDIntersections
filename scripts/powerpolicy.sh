581f8bba6a9f10613f76aa83d939673361d5dbc9
Daniel Lemire <dlemire@fastpfor.(none)>
2013-08-26 20:38:34 -0400

    Added docs.

--- scripts/powerpolicy.sh
+++ scripts/powerpolicy.sh
@@ -2,7 +2,7 @@
 # taken from http://hbfs.wordpress.com/2013/06/18/fast-path-finding-part-ii/
 # might require sudo apt-get install cpufrequtils
 # invoke with performance or ondemand 
-# type cpufreq-info to check results
+# type cpufreq-info or "cat /proc/cpuinfo" to check results
 # enumerate found CPUs
 cpus=$( grep processor /proc/cpuinfo | cut -d: -f 2 )
  
