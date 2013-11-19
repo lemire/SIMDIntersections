29b488b8909e03bfe3dd7a47263f5007f09b60cb
Daniel Lemire <lemire@gmail.com>
2013-11-19 00:00:10 -0500

    Updating intersection hybrid

--- include/hybridintersection.h
+++ include/hybridintersection.h
@@ -49,19 +49,13 @@ size_t danielshybridintersection(const uint32_t * set1,
         const size_t length1, const uint32_t * set2, const size_t length2, uint32_t *out) {
     if ((length1==0) or (length2 == 0)) return 0;
 
-    if ((100 * length1 <= length2) or (100 * length2 <= length1)) {
+    if ((50 * length1 <= length2) or (50 * length2 <= length1)) {
             if (length1 < length2)
                 return danfarfar_medium(set1, length1, set2, length2,out);
             else
                 return danfarfar_medium(set2, length2, set1, length1,out);
     }
 
-    if ((50 * length1 <= length2) or (50 * length2 <= length1)) {
-            if (length1 < length2)
-                return danfar_medium(set1, length1, set2, length2,out);
-            else
-                return danfar_medium(set2, length2, set1, length1,out);
-    }
    if (length1 < length2)
         return match_v4_f2_p0(set1, length1, set2, length2, out);
     else
